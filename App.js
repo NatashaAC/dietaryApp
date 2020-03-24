6ddee340bccf142b02f2067c2f77857d14141a53
Natasha Chambers <natashaagathachambers@gmail.com>
2020-03-24 18:11:30 -0400

    updating onboarding screens

--- App.js
+++ App.js
@@ -4,16 +4,14 @@ import { createStackNavigator } from 'react-navigation-stack';
 import LoginScreen from './components/screens/form/loginscreen';
 import RegisterScreen from './components/screens/form/registerscreen';
 import HomeScreen from './components/screens/main/homescreen';
-import Onboarding from './components/platform/index.android';
 
 const stackNavigator = createStackNavigator({
   Login: LoginScreen,
   Register: RegisterScreen,
-  Home: HomeScreen,
-  Onboarding: Onboarding
+  Home: HomeScreen
 },
 {
-  initialRouteName: 'Onboarding'
+  initialRouteName: 'Login'
 }
 );
 
--- components/onboarding/onboardingscreen.js
+++ components/onboarding/onboardingscreen.js
@@ -2,34 +2,31 @@ import React, { Component } from 'react';
 import { StyleSheet, View, Text, Image } from 'react-native';
 
 import Button from '../onboarding/button';
-import Swiper from '../onboarding/swiper';
 
 export default class Screens extends Component {
     render() {
         return (
-            
-                <View style={styles.slide}>
-                    <View style={styles.bg}>
-                        <Image style={styles.logo} source={require('../../assets/logo.png')}></Image>
-                        <Text style={styles.text}>Having trouble finding a recipe that suits you, don't worry Nutridish can help you with that.</Text>
-                    </View>
+            <View style={styles.slide}>
+                <View style={styles.bg}>
+                    <Image style={styles.logo} source={require('../../assets/logo.png')}></Image>
+                    <Text style={styles.text}>Having trouble finding a recipe that suits you, don't worry Nutridish can help you with that.</Text>
                 </View>
+                <Button text='Continue'  />
+            </View>
 
-                <View style={styles.slide}>
-                    <View style={styles.bg}>
-                        <Text style={style.headerText}>How to Use the App</Text>
-                        <Text style={styles.text}>Having trouble finding a recipe that suits you, don't worry Nutridish can help you with that.</Text>
-                    </View>
-                </View>
+            // <View style={styles.slide}>
+            //     <View style={styles.bg}>
+            //         <Text style={style.headerText}>How to Use the App</Text>
+            //         <Text style={styles.text}>Having trouble finding a recipe that suits you, don't worry Nutridish can help you with that.</Text>
+            //     </View>
+            // </View>
 
-                <View style={styles.slide}>
-                    <View style={styles.bg}>
-                        <Text >Let's Get Started</Text>
-                        <Text style={styles.text}>Having trouble finding a recipe that suits you, don't worry Nutridish can help you with that.</Text>
-                    </View>
-                </View>
-                <Button title='Continue'  />
-            
+            // <View style={styles.slide}>
+            //     <View style={styles.bg}>
+            //         <Text >Let's Get Started</Text>
+            //         <Text style={styles.text}>Having trouble finding a recipe that suits you, don't worry Nutridish can help you with that.</Text>
+            //     </View>
+            // </View> 
         );
     }
 }
