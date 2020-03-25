e06663f457ccd4d137bd90da0d78195d07fb4a18
Natasha Chambers <natashaagathachambers@gmail.com>
2020-03-24 22:32:41 -0400

    creating switch and bottom tab navigation

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { creeateBottomTabNavigator, createBottomTabNavigator } from 'react-navigation-tabs';
 
 import LoginScreen from './components/screens/form/loginscreen';
 import RegisterScreen from './components/screens/form/registerscreen';
 import HomeScreen from './components/screens/main/homescreen';
+
+import MealPlanScreen from './components/screens/main/mealplanscreen';
+import RecipeScreen from './components/screens/main/recipescreen';
+import SavedRecipeScreen from './components/screens/main/savedrecipescreen';
 
 const stackNavigator = createStackNavigator({
   Login: LoginScreen,
@@ -14,5 +19,10 @@ const stackNavigator = createStackNavigator({
   initialRouteName: 'Login'
 }
 );
+
+const homeTabNavigator = createBottomTabNavigator({
+  Home: HomeScreen,
+  Meal: Meal
+})
 
 export default createAppContainer(stackNavigator);
