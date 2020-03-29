cbf2fb12e2b13cb16536deed834cfc880e0c0e6d
Natasha Chambers <natashaagathachambers@gmail.com>
2020-03-28 20:32:22 -0400

    making changes to the api call

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
+import OnboardingScreens from './components/screens/onboarding/onboardingscreen';
 import LoginScreen from './components/screens/form/loginscreen';
 import RegisterScreen from './components/screens/form/registerscreen';
-import HomeScreen from './components/screens/main/homescreen';
 
+import HomeScreen from './components/screens/main/homescreen';
 import RecipeScreen from './components/screens/main/recipescreen';
 import SavedRecipeScreen from './components/screens/main/savedrecipescreen';
-import OnboardingScreens from './components/onboarding/onboardingscreen';
 
 const switchNavigator = createSwitchNavigator({
   Onboarding: OnboardingScreens,
@@ -19,10 +18,6 @@ const switchNavigator = createSwitchNavigator({
   initialRouteName: 'Onboarding'
 }
 );
-
-// const dashboardStackNavigator = createStackNavigator({
-//   homeTabNavigator: homeTabNavigator
-// });
 
 const homeTabNavigator = createBottomTabNavigator({
   Home: HomeScreen,
--- components/screens/main/recipescreen.js
+++ components/screens/main/recipescreen.js
@@ -11,48 +11,44 @@ export default class RecipeScreen extends Component {
     recipeData: {}
   }
 
-  componentDidMount() {
+  recipeSearch = () => {
+    const recipeName = this.state.searchRecipe.toLowerCase();
 
-    recipeSearch = () => {
-      const recipeName = this.state.searchRecipe.toLowerCase();
-  
-      // API call
-      const axios = require("axios");
-  
-      axios({
-          "method":"GET",
-          "url":"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search",
-          "headers":{
-          "content-type":"application/octet-stream",
-          "x-rapidapi-host":"spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-          "x-rapidapi-key":"28ef96556amsh63000c2e224a8cfp1806e9jsn1a1938144972"
-          },"params":{
-          "number":"1",  // Number of recipes retrieved from the database
-          "query": recipeName // User's input 
-          }
-          })
-          .then((response)=>{
-            console.log(response.data)
-  
-            // this.setState({
-            //   recipeData: response.data
-            // })
-          })
-          .catch((error)=>{
-            console.log(error)
-          })
-    }
+    // API call
+    Axios = require("axios");
 
+    Axios({
+        "method":"GET",
+        "url":"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search",
+        "headers":{
+        "content-type":"application/octet-stream",
+        "x-rapidapi-host":"spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+        "x-rapidapi-key":"28ef96556amsh63000c2e224a8cfp1806e9jsn1a1938144972"
+        },"params":{
+        "number":"1",  // Number of recipes retrieved from the database
+        "query": recipeName // User's input 
+        }
+        })
+        .then((response)=>{
+          // console.log(response.data)
+          
+          var data = response.data[0] ? response.data[0] : false
+          this.setState({
+            recipeData: data
+          })
+        })
+        .catch((error)=>{
+          console.log(error)
+        })
   }
 
-
-  // renderContent = () => {
-  //   if(this.state.recipeData) {
-  //   return <View><Text>{this.state.recipeData.title}</Text></View>
-  //   } else {
-  //     console.log('Recipe not found')
-  //   }
-  // }
+  renderContent = () => {
+    if(this.state.recipeData) {
+    return <View><Text style={{color: '#000'}}>{this.state.recipeData.title}</Text></View>
+    } else {
+      console.log('Recipe not found')
+    }
+  }
 
   render() {
     return (
@@ -79,7 +75,7 @@ export default class RecipeScreen extends Component {
           onSubmitEditing={this.recipeSearch}
           recipeSearch = {this.recipeSearch}
            />
-           {/* {this.renderContent()} */}
+           {this.renderContent()}
       </View>
     );
   }
